94d8f40c09ec89313e11dd123496c9aa780ab0e8
Karolis Ramanauskas <hello@karolisram.com>
2017-10-23 11:57:11 +0300

    Add Lesson 9 (ERC20 Token implementation)

--- lesson-9/DummyToken.sol
+++ lesson-9/DummyToken.sol
@@ -30,10 +30,10 @@ contract DummyToken {
 	// Send _value amount of tokens from address _from to address _to
 	function transferFrom(address _from, address _to, uint _value) returns (bool success) {
 		// Unsuccessful transaction
-		if (_value <= 0 || allowance[_from][msg.sender] < _value || balances[_from] < _value) return false;
+		if (_value <= 0 || allowance(_from, msg.sender) < _value || balances[_from] < _value) return false;
 
 		balances[_from] -= _value;
-		allowance[_from][msg.sender] -= _value;
+		approved[_from][msg.sender] -= _value;
 		balances[_to] += _value;
 
 		return true;
@@ -45,7 +45,7 @@ contract DummyToken {
 		if (_value <= 0 || balances[msg.sender] < _value) return false;
 
 		// approved[owner][spender] - allowed _value
-		approve[msg.sender][_spender] = _value;
+		approved[msg.sender][_spender] = _value;
 
 		return true;
 	}
@@ -60,4 +60,14 @@ contract DummyToken {
 
 	// Triggered whenever approve(address _spender, uint256 _value) is called.
 	event Approval(address indexed _owner, address indexed _spender, uint _value);
+
+	// Custom (not ERC20 functions)
+	function mine() {
+		balances[msg.sender] += 1;
+		supply += 1;
+	}
+
+	function myBalance(uint) {
+		return balances[msg.sender];
+	}
 }
