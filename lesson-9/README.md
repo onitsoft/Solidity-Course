9be1bb82fcac7158180697f9a7f54f081cb21173
Karolis Ramanauskas <hello@karolisram.com>
2017-10-23 14:17:48 +0300

    Lesson 9 (Truffle example)

--- lesson-9/README.md
+++ lesson-9/README.md
@@ -1,3 +1,27 @@
 # Lesson 9 - ERC20 Token Implementation
 
 In this lesson we implement all the functions in ERC20 Token Standard Interface and prepare an ERC20 compliant token (only lacking events). Find full code for it at [lesson-9/DummyToken.sol](https://github.com/onitsoft/BitDegree-Solidity-Course/blob/master/lesson-9/DummyToken.sol).
+
+## Deploying to testrpc
+
+Full steps can be found in the instructional video but these are the provisional steps to compile and deploy contracts on testrpc using Truffle.
+
+1. Create and go to new directory - `mkdir dummy-token-truffle && cd dummy-token-truffle`.
+2. Initiate Truffle project - `truffle init`.
+3. Add `DummyToken.sol` to contracts directory.
+4. Remove all other contracts besides `Migrations.sol`.
+5. Adjust `migrations/2_deploy_contracts.js` to only leave `DummyToken.sol` to be deployed.
+6. Make sure testrpc node is running. It can be launched via `testrpc` command from a new terminal window.
+7. From Truffle project root compile the contracts by typing `truffle compile`.
+8. Then deploy the contract with `truffle deploy`.
+
+## Using Truffle console
+
+To test testrpc we need to connect to truffle console by Typing `truffle console`. From there we can interact with our contract and testrpc node.
+
+### Useful commands
+
+- `web3.eth.accounts` - retrieve all accounts created by testrpc node.
+- `DummyToken.deployed().then(function(instance) {app=instance;})` - retrieve instance of our contract.
+- `app.getterFunction.call({from: web3.eth.accounts[0]})` - call getter function that does not alter blockchain.
+- `app.setterFunction("some param", {from: web3.eth.accounts[1]})` - call setter functions that alters blockchain state.
---
+++ lesson-9/dummy-token-truffle/contracts/DummyToken.sol
@@ -0,0 +1,68 @@
+pragma solidity ^0.4.18;
+
+contract DummyToken {
+	mapping(address => uint) balances;
+	mapping(address => mapping(address => uint)) approved;
+	uint supply;
+
+	// Get the total token supply in circulation
+	function totalSupply() constant returns (uint totalSupply) {
+		return supply;
+	}
+
+	// Get the account balance of another account with address _owner
+	function balanceOf(address _owner) constant returns (uint balance) {
+		return balances[_owner];
+	}
+
+	// Send _value amount of tokens to address _to
+	function transfer(address _to, uint _value) returns (bool success) {
+		// Unsuccessful transaction
+		if (_value <= 0 || balances[msg.sender] < _value) return false; 
+
+		balances[msg.sender] -= _value;
+		balances[_to] += _value;
+
+		return true;
+	}
+
+	// Send _value amount of tokens from address _from to address _to
+	function transferFrom(address _from, address _to, uint _value) returns (bool success) {
+		// Unsuccessful transaction
+		if (_value <= 0 || allowance(_from, msg.sender) < _value || balances[_from] < _value) return false;
+
+		balances[_from] -= _value;
+		approved[_from][msg.sender] -= _value;
+		balances[_to] += _value;
+
+		return true;
+	}
+
+	// Allow _spender to withdraw from your account, multiple times, up to the _value amount.
+	// If this function is called again it overwrites the current allowance with _value.
+	function approve(address _spender, uint _value) returns (bool success) {
+		if (_value <= 0 || balances[msg.sender] < _value) return false;
+
+		// approved[owner][spender] - allowed _value
+		approved[msg.sender][_spender] = _value;
+
+		return true;
+	}
+
+	// Returns the amount which _spender is still allowed to withdraw from _owner
+	function allowance(address _owner, address _spender) constant returns (uint remaining) {
+		return approved[_owner][_spender];
+	}
+
+	// Triggered when tokens are transferred.
+	event Transfer(address indexed _from, address indexed _to, uint _value);
+
+	// Triggered whenever approve(address _spender, uint256 _value) is called.
+	event Approval(address indexed _owner, address indexed _spender, uint _value);
+
+	// Custom (not ERC20 functions)
+	function mine() {
+		balances[msg.sender] += 1;
+		supply += 1;
+	}
+}
